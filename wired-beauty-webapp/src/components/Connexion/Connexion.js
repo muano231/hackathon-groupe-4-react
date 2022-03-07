33aae79943c65925b77f7f2949cafbff61e8781f
Aliaksandra Skachkova <skach.alexandra@gmail.com>
2022-03-07 18:28:52 +0100

    add routing

---
+++ wired-beauty-webapp/src/components/Connexion/Connexion.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Connexion.scss";
+
+function Connexion() {
+  return <div className="Connexion">Connexion Component</div>;
+}
+
+Connexion.propTypes = {};
+
+Connexion.defaultProps = {};
+
+export default Connexion;

f43959864a30396f28c0144544c78c7b93e4a16e
Leo Terras <l.terras@sciencesu-lyon.fr>
2022-03-08 09:48:22 +0100

    add dashboard

--- wired-beauty-webapp/src/App.js
+++ wired-beauty-webapp/src/App.js
@@ -2,13 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Connexion from "./components/Connexion/Connexion";
+import Dashboard from "./components/Dashboard/Dashboard";
 import "./base.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Connexion />}>
+        <Route path="/signin" element={<Connexion />}>
+        <Route path="/dashboard" element={<Dashboard />}>
+        {/* <Route path="/signup" element={<SignUp />}> */}
           {/* <Route index element={<Home />} />
         <Route path="blogs" element={<Blogs />} />
         <Route path="contact" element={<Contact />} />

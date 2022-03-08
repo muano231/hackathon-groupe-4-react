03dd6f8e8f375443ed74bd6966d1b9be776c1728
Leo Terras <l.terras@sciencesu-lyon.fr>
2022-03-08 09:49:09 +0100

    modif dashboard

--- wired-beauty-webapp/src/components/Dashboard/Dashboard.js
+++ wired-beauty-webapp/src/components/Dashboard/Dashboard.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Dashboard.scss';
+import { users } from '../datas/Users';
 
-const Dashboard = () => (
-  <div className="Dashboard">
-    Dashboard Component
-  </div>
-);
+function Dashboard() {
 
-Dashboard.propTypes = {};
+  const Calculate_age = (user_birthdate) => {
+    var today = new Date();
+    var birthdate = new Date(user_birthdate);
+    var age = today.getFullYear() - birthdate.getFullYear();
+    var m = today.getMonth() - birthdate.getMonth();
+    if(m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
+      age--
+    }
+    return age;
+  }
 
-Dashboard.defaultProps = {};
+  return(
+    <div>
+      <h1></h1>
+      <table>
+        <tr>
+          <th>age</th>
+          <th>height</th>
+          <th>weight</th>
+          <th>town</th>
+        </tr>
+
+        {users.map((user, index) => (
+          <tr data-index={index}>
+            <td>{Calculate_age(user.birthdate)}</td>
+            <td>{user.height}</td>
+            <td>{user.weight}</td>
+            <td>{user.town}</td>
+          </tr>
+        ))}
+
+      </table>
+    </div>
+  )
+}
 
 export default Dashboard;

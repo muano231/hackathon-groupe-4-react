433a0b7cb17bfa70dbb40bd57e4c793ef54c3324
Leo Terras <l.terras@sciencesu-lyon.fr>
2022-03-08 09:55:02 +0100

    modif routes

--- wired-beauty-webapp/src/App.js
+++ wired-beauty-webapp/src/App.js
@@ -9,14 +9,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signin" element={<Connexion />}>
-        <Route path="/dashboard" element={<Dashboard />}>
-        {/* <Route path="/signup" element={<SignUp />}> */}
+        <Route path="/signin" element={<Connexion />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
           {/* <Route index element={<Home />} />
         <Route path="blogs" element={<Blogs />} />
         <Route path="contact" element={<Contact />} />
         <Route path="*" element={<NoPage />} /> */}
-        </Route>
       </Routes>
     </BrowserRouter>
   );

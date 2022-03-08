38c7fb1f46905ab0f04693de42c1a30de49932ab
Leo Terras <l.terras@sciencesu-lyon.fr>
2022-03-08 09:34:38 +0100

    dashboard

--- wired-beauty-webapp/src/App.js
+++ wired-beauty-webapp/src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Connexion from "./components/Connexion/Connexion";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Connexion />}>
+        <Route path="/" element={<Dashboard />}>
           {/* <Route index element={<Home />} />
         <Route path="blogs" element={<Blogs />} />
         <Route path="contact" element={<Contact />} />

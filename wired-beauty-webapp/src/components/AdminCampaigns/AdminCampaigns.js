10449171e8bf2f4db241323759b6f1f38ec62312
Aliaksandra Skachkova <skach.alexandra@gmail.com>
2022-03-10 18:55:00 +0100

    add creation session form

--- wired-beauty-webapp/src/components/AdminCampaigns/AdminCampaigns.js
+++ wired-beauty-webapp/src/components/AdminCampaigns/AdminCampaigns.js
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 import "./AdminCampaigns.scss";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
+import { Formik, Form, Field } from "formik";
 
 class AdminCampaigns extends React.Component {
   currentDate = new Date();
 
   constructor(props) {
     super(props);
+    this.createNewSession = false;
     this.state = {
       error: null,
       isLoaded: false,
@@ -19,7 +21,7 @@ class AdminCampaigns extends React.Component {
   componentDidMount() {
     // console.log(JSON.parse(sessionStorage.getItem("user")));
     const token = JSON.parse(sessionStorage.getItem("user")).access_token;
-    fetch(process.env.REACT_APP_API + "api/studies", {
+    fetch(process.env.REACT_APP_API + "/api/studies", {
       method: "get",
       headers: {
         Accept: "application/json",
@@ -43,6 +45,12 @@ class AdminCampaigns extends React.Component {
         }
       );
   }
+
+  creationSession = () => {
+    console.log("hello");
+    this.createNewSession = true;
+  };
+
   render() {
     const { error, isLoaded, items } = this.state;
     if (error) {
@@ -72,7 +80,7 @@ class AdminCampaigns extends React.Component {
                       </p>
                       {/* <p>{session.availability_start}</p> */}
                       <p>
-                        <b>Date de fin : </b>
+                        <b>Final date: </b>
                         {session.availability_end}
                       </p>
                       {/* <Link
@@ -83,9 +91,8 @@ class AdminCampaigns extends React.Component {
                         to={"/study/" + session.study_id}
                         state={session.study_id}
                       >
-                        <button className="card-button">
-                          Participer <FaArrowRight />
-                        </button>
+                        <button>Modify</button>
+                        <button>Delete</button>
                       </Link>
                     </div>
                   );
@@ -93,6 +100,84 @@ class AdminCampaigns extends React.Component {
               </div>
             </div>
           ))}
+          {/* CREATION DE LA NOUVELLE SESSION */}
+          <div>
+            Session
+            <button onclick={this.createNewSession === true}>
+              Create new session
+            </button>
+          </div>
+          {this.createNewSession ? (
+            <Formik
+              initialValues={{
+                study_id: "",
+                availability_start: "",
+                availability_end: "",
+                description: "",
+                questions: "",
+              }}
+              onSubmit={async (values) => {
+                console.log(values);
+                const token = JSON.parse(
+                  sessionStorage.getItem("user")
+                ).access_token;
+                fetch(process.env.REACT_APP_API + "/api/sessions", {
+                  method: "post",
+                  body: JSON.stringify(values),
+                  headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                  },
+                })
+                  .then((res) => res.json())
+                  .then(
+                    (result) => {
+                      console.log("result", result);
+                    },
+                    (error) => {
+                      console.log(error);
+                    }
+                  );
+              }}
+            >
+              <Form>
+                <Field
+                  type="text"
+                  className="input"
+                  name="study_id"
+                  required
+                ></Field>
+                <Field
+                  type="text"
+                  className="input"
+                  name="availability_start"
+                  required
+                ></Field>
+                <Field
+                  type="text"
+                  className="input"
+                  name="availability_end"
+                  required
+                ></Field>
+                <Field
+                  type="text"
+                  className="input"
+                  name="description"
+                  required
+                ></Field>
+                <Field
+                  type="text"
+                  className="input"
+                  name="questions"
+                  required
+                ></Field>
+                <button type="submit">SEND</button>
+              </Form>
+            </Formik>
+          ) : (
+            ""
+          )}
         </div>
       );
     }
